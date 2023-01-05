8c036a3f4e1a2f921cba77c837e20f8b031bb9b2
turissssst <turisssst@hotmail.com>
2023-01-05 12:42:53 +0200

    Add beautifier

--- README.md
+++ README.md
@@ -2,16 +2,19 @@
 
 ![](./images/1.gif)
 
-# It is not safe!
+# It is not safe! 
 ![](./images/2.gif)
 
 # TODO
 - [ ] Make it safe
 - [ ] Make functions list
 - [ ] Make authimatic funtions finder
+- [X] beautifier
 - [ ] ....
 
 # Credits
-codemirror - [codemirror5](hhttps://github.com/codemirror/codemirror5)
+codemirror - [codemirror5](https://github.com/codemirror/codemirror5)
 
 atakde - [online-code-editor](https://github.com/atakde/online-code-editor)
+
+beautifier - [js-beautify](https://github.com/beautify-web/js-beautify)
--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ const dataLogEl = document.querySelector("[data-log]");
 const addButtonEl = document.querySelector("#add");
 const evalButtonEl = document.querySelector("#eval");
 const clearButtonEl = document.querySelector("#clear");
+const beautifierButtonEl = document.querySelector("#beautifier");
 
 const defaultEditorSettings = {
   styleActiveLine: true,
@@ -50,6 +51,31 @@ function evalInContext(context, js) {
   }
 }
 
+function beautify(source) {
+
+  let opts = {
+    brace_style: "collapse",
+    break_chained_methods: false,
+    comma_first: false,
+    e4x: false,
+    end_with_newline: false,
+    indent_char: " ",
+    indent_empty_lines: false,
+    indent_inner_html: false,
+    indent_scripts: "normal",
+    indent_size: "4",
+    jslint_happy: false,
+    keep_array_indentation: false,
+    max_preserve_newlines: "5",
+    preserve_newlines: true,
+    space_before_conditional: true,
+    unescape_strings: false,
+    wrap_line_length: "0",
+  };
+
+  return js_beautify(source, opts);
+}
+
 addButtonEl.addEventListener("click", () => {
   let selectionText = jsEditor.getSelection().toString();
   let result = evalInContext(PRIVATE_CONTEXT, selectionText);
@@ -77,4 +103,10 @@ clearButtonEl.addEventListener("click", () => {
   CONTEXT_CODE = "";
   PRIVATE_CONTEXT = {};
   
+});
+
+beautifierButtonEl.addEventListener("click", () => {
+  
+  let result = beautify(jsEditor.getValue());
+  jsEditor.setValue(result);
 });
